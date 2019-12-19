259a2f05f09bb312ce346faacbd13582814fdb0f
kayo <kayo.riccelo@hotmail.com>
2019-12-19 14:42:06 -0300

    Alterando estrutura de dados da api para a de previsão de 5 dias;

--- src/pages/Weather/index.js
+++ src/pages/Weather/index.js
@@ -6,18 +6,39 @@ import { StyledRoot, StyledMain } from './styled';
 
 export default function Weather() {
     const data = {
-        "coord": { "lon": -0.13, "lat": 51.51 },
-        "weather": [{ "id": 300, "main": "Drizzle", "description": "light intensity drizzle", "icon": "09d" }],
-        "base": "stations",
-        "main": { "temp": 280.32, "pressure": 1012, "humidity": 81, "temp_min": 279.15, "temp_max": 281.15 },
-        "visibility": 10000,
-        "wind": { "speed": 4.1, "deg": 80 },
-        "clouds": { "all": 90 },
-        "dt": 1485789600,
-        "sys": { "type": 1, "id": 5091, "message": 0.0103, "country": "GB", "sunrise": 1485762037, "sunset": 1485794875 },
-        "id": 2643743,
-        "name": "London",
-        "cod": 200
+        "city": {
+            "id": 1851632,
+            "name": "Shuzenji",
+            "coord": {
+                "lon": 138.933334,
+                "lat": 34.966671
+            },
+            "country": "JP",
+            "timezone": 32400,
+            "cod": "200",
+            "message": 0.0045,
+            "cnt": 38,
+            "list": [
+                {
+                    "dt": 1406106000,
+                    "main": {
+                        "temp": 298.77,
+                        "temp_min": 298.77,
+                        "temp_max": 298.774,
+                        "pressure": 1005.93,
+                        "sea_level": 1018.18,
+                        "grnd_level": 1005.93,
+                        "humidity": 87,
+                        "temp_kf": 0.26
+                    },
+                    "weather": [{ "id": 804, "main": "Clouds", "description": "overcast clouds", "icon": "04d" }],
+                    "clouds": { "all": 88 },
+                    "wind": { "speed": 5.71, "deg": 229.501 },
+                    "sys": { "pod": "d" },
+                    "dt_txt": "2014-07-23 09:00:00"
+                }
+            ]
+        }
     };
 
     return (
